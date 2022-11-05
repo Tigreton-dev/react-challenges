06b8d14f2ef9bf6da0d652a09232d843d9dd17ac
Javi Sanchez <javier.san.mail@gmail.com>
2022-11-05 16:29:42 +0100

    add conde section with code image

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from 'react'
-import {challenge1} from './challengeInfo'
+import { challenge1 } from './challengeInfo'
+import Challenge1 from './challenges/Challenge1'
 import './App.css'
 
 function App() {
-	const [count, setCount] = useState(0)
+	const [step, setStep] = useState(1)
 
 	useEffect(() => {
-		document.getElementById('aa').innerHTML = challenge1
-	}, [])
+		if (step === 0) document.getElementById('aa').innerHTML = challenge1
+	}, [step])
+
+	const clickHandler = (val: number) => {
+		setStep(val)
+	}
 
 	return (
 		<div className=" flex max-w-6xl gap-6 h-full">
@@ -31,11 +36,18 @@ function App() {
 				<p>adgdfgdfg</p>
 			</div>
 			<div className="grow  bg-fuchsia-500 p-5">
-
-				<button className='bg-white px-5 m-5'>Description</button>
-				<button className='bg-white px-5 m-5'>Render Solution</button>
-				<button className='bg-white px-5 m-5'>Code</button>
-				<div id='aa' className='text-left bg-indigo-700'></div>
+				<button className='bg-white px-5 m-5' onClick={() => clickHandler(0)}>Description</button>
+				<button className='bg-white px-5 m-5' onClick={() => clickHandler(1)}>Demo</button>
+				<button className='bg-white px-5 m-5' onClick={() => clickHandler(2)}>Code</button>
+				{step === 0 && (
+					<div id='aa' className='text-left bg-indigo-700'></div>
+				)}
+				{step === 1 && (
+					<Challenge1 />
+				)}
+				{step === 2 && (
+					<img src={'./Snap.png'} ></img>
+				)}
 
 			</div>
 		</div >
