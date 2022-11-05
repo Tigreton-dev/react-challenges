73a66ab76b73d32f4fe38144220477733a98078e
Javi Sanchez <javier.san.mail@gmail.com>
2022-11-05 15:55:34 +0100

    move challenge description out of component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
 import { useEffect, useState } from 'react'
+import {challenge1} from './challengeInfo'
 import './App.css'
 
 function App() {
 	const [count, setCount] = useState(0)
 
 	useEffect(() => {
-		document.getElementById('aa').innerHTML = `
-<pre><code>
-/**
- * 🇬🇧
- * Given this API:
- *  - https://openlibrary.org/developers/api - Open Library API
- *
- * Create a way to input a text.
- * Use Search API with the input to list the books found.
- * We only want to show the title, first year published and,
- * finally, the cover.
- * 
- * Everything should be centered in the screen.
- * The list of books should be horizontal and scrollable. 
- * 
- * > Limit the results to 25.
- * > Can we filter books with only one Edition Count?
- * > Can we grab only the fields we need to optimize
- *   the network request?
- * > Can we debounce the fetch of the data after input to avoid
- *   firing too much requests while typing?
- */
-</pre></code>
-`
-
+		document.getElementById('aa').innerHTML = challenge1
 	}, [])
 
 	return (
---
+++ src/challengeInfo.ts
@@ -0,0 +1,24 @@
+export const challenge1 = `
+<pre><code>
+/**
+ * 🇬🇧
+ * Given this API:
+ *  - https://openlibrary.org/developers/api - Open Library API
+ *
+ * Create a way to input a text.
+ * Use Search API with the input to list the books found.
+ * We only want to show the title, first year published and,
+ * finally, the cover.
+ * 
+ * Everything should be centered in the screen.
+ * The list of books should be horizontal and scrollable. 
+ * 
+ * > Limit the results to 25.
+ * > Can we filter books with only one Edition Count?
+ * > Can we grab only the fields we need to optimize
+ *   the network request?
+ * > Can we debounce the fetch of the data after input to avoid
+ *   firing too much requests while typing?
+ */
+</pre></code>
+`
