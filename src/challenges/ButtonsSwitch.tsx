3dec65a0d796dded16fdc91a0c48920c8ae6a41e
Javi Sanchez <javier.san.mail@gmail.com>
2022-11-06 16:06:38 +0100

    fix style at buttonSwitch component

--- src/challenges/ButtonsSwitch.tsx
+++ src/challenges/ButtonsSwitch.tsx
@@ -22,7 +22,7 @@ const ButtonSwitch = ({ setStep }) => {
     }
 
     return (
-        <div className='border-solid border border-neutral-800 mb-8 bg-black rounded-xl mt-10'>
+        <div className='border-solid border border-neutral-800 mb-8 bg-black rounded-xl mt-10 text-center'>
             <button ref={ref} className='bg-blue-600 px-5 m-5 text-white p-1 rounded-md' onClick={(e) => clickHandler(e, 0)}>Description</button>
             <button className='bg-neutral-800 px-5 m-5 text-white p-1 rounded-md' onClick={(e) => clickHandler(e, 1)}>Demo</button>
             <button className='bg-neutral-800 px-5 m-5 text-white p-1 rounded-md' onClick={(e) => clickHandler(e, 2)}>Code</button>
