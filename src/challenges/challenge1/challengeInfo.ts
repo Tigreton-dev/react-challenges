8a6d1420aa4fc03459c7caa22932d3c04bcb45e6
Javi Sanchez <javier.san.mail@gmail.com>
2022-11-12 16:31:11 +0100

    update styles

--- src/challenges/challenge1/challengeInfo.ts
+++ src/challenges/challenge1/challengeInfo.ts
@@ -1,24 +1,21 @@
 export const description = `
-<pre><code>/**
- * 🇬🇧
- * Given this API:
- *  - https://openlibrary.org/developers/api - Open Library API
- *
- * Create a way to input a text.
- * Use Search API with the input to list the books found.
- * We only want to show the title, first year published and,
- * finally, the cover.
- * 
- * Everything should be centered in the screen.
- * The list of books should be horizontal and scrollable. 
- * 
- * > Limit the results to 25.
- * > Can we filter books with only one Edition Count?
- * > Can we grab only the fields we need to optimize
- *   the network request?
- * > Can we debounce the fetch of the data after input to avoid
- *   firing too much requests while typing?
- */
+<h1 class='text-xl'>Blink Challenge</h1>
+<pre><code>
+------------------------------------------------------------------------------------------------
+Given this API: https://openlibrary.org/developers/api - Open Library API
+Create a way to input a text.
+Use Search API with the input to list the books found.
+We only want to show the title, first year published and,
+finally, the cover.
+
+Everything should be centered in the screen.
+The list of books should be horizontal and scrollable. 
+
+- Limit the results to 25.
+- Can we filter books with only one Edition Count?
+- Can we grab only the fields we need to optimize the network request?
+- Can we debounce the fetch of the data after input to avoid firing too much requests while 
+  typing?
 </pre></code>
 `;
 
