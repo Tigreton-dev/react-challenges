b6240c24a2252cc2b11d3ad704ff71524e96ab02
Javi Sanchez <javier.san.mail@gmail.com>
2022-11-25 17:29:18 +0100

    add toggle challenge

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -9,12 +9,7 @@ const Menu = ({updateChallenge}) => {
             <p onClick={() => updateChallenge('challenge4')} className='p-2 hover:bg-neutral-800 hover:text-neutral-50 rounded-md m-2 cursor-pointer'>Resize Div</p>
             <p  onClick={() => updateChallenge('challenge5')} className='p-2 hover:bg-neutral-800 hover:text-neutral-50 rounded-md m-2 cursor-pointer'>FlexBox</p>
             <p onClick={() => updateChallenge('challenge6')} className='p-2 hover:bg-neutral-800 hover:text-neutral-50 rounded-md m-2 cursor-pointer'>Form</p>
-
-            <p className='p-2 hover:bg-neutral-800 hover:text-neutral-50 rounded-md m-2 cursor-pointer'>Blink Challenge</p>
-            <p className='p-2 hover:bg-neutral-800 hover:text-neutral-50 rounded-md m-2 cursor-pointer'>Interval Timer</p>
-            <p className='p-2 hover:bg-neutral-800 hover:text-neutral-50 rounded-md m-2 cursor-pointer'>UseContext pattern</p>
-            <p className='p-2 hover:bg-neutral-800 hover:text-neutral-50 rounded-md m-2 cursor-pointer'>Fetching Data</p>
-            <p className='p-2 hover:bg-neutral-800 hover:text-neutral-50 rounded-md m-2 cursor-pointer'>Form validation</p>
+            <p onClick={() => updateChallenge('challenge7')} className='p-2 hover:bg-neutral-800 hover:text-neutral-50 rounded-md m-2 cursor-pointer'>Toggle</p>
         </div>
     )
 }
